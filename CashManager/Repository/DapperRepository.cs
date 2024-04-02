ae9006b76fe70cb2bab83b3c541a0ff1ad683c52
HasiukI <136508423+HasiukI@users.noreply.github.com>
2024-04-02 20:53:43 +0300

    install Dapper and create config file

---
+++ CashManager/Repository/DapperRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashManager.Repository
+{
+    internal class DapperRepository
+    {
+        private readonly string _sqlConnection = String.Empty;
+
+        public DapperRepository(string sqlConnection) { 
+            _sqlConnection = sqlConnection;
+        }
+
+
+    }
+}
