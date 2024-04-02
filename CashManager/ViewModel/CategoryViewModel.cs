701c9eb591c6f7c83d2546aae0470f61a1eb56c2
HasiukI <136508423+HasiukI@users.noreply.github.com>
2024-04-02 20:44:49 +0300

    create inViewModel method for create category

---
+++ CashManager/ViewModel/CategoryViewModel.cs
@@ -0,0 +1,46 @@
+﻿using CashManager.Model;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CashManager.ViewModel
+{
+    internal class CategoryViewModel :ViewModelBase
+    {
+        public CategoryViewModel() { 
+           
+           
+            List<Category> categories = new List<Category>();
+
+            categories.Add(new Category() { Id = 1, Name = "ct1", Price = 100 });
+            categories.Add(new Category() { Id = 1, Name = "ct2", Price = 100 });
+
+            _categories = new ObservableCollection<Category>(categories);
+        }
+
+
+
+        private ObservableCollection<Category> _categories { get; set; }
+        public ObservableCollection<Category> Categories { get { return _categories; } }
+
+
+        private string _nameCategory;
+        public string NameCategory { get => _nameCategory; set => _nameCategory = value; }
+
+        private int _priceCategory;
+        public int PriceCategory { get=> _priceCategory; set => _priceCategory = value; }
+       
+
+        public ICommand CreateCategory
+        {
+            get => new RelayCommand(() => _categories.Add(new Category() { Id = 1, Name = _nameCategory, Price = _priceCategory }));
+        }
+
+    }
+}
--- CashManager/ViewModel/ViewModelBase.cs
+++ CashManager/ViewModel/ViewModelBase.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CashManager.ViewModel
 {
-    internal class ViewModelBase 
+    internal class ViewModelBase : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void onPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
