0fc0600752cef47ff8ebb64f04ea8e7b6260f0c8
HasiukI <136508423+HasiukI@users.noreply.github.com>
2024-04-02 19:51:36 +0300

    Add project files.

---
+++ CashManager/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CashManager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
