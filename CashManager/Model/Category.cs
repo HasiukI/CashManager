3fafd158f1f99d2a7acd6fbb5c1a4febc7ce2885
HasiukI <136508423+HasiukI@users.noreply.github.com>
2024-04-02 21:04:18 +0300

    update Model Category

--- CashManager/Model/Category.cs
+++ CashManager/Model/Category.cs
@@ -10,6 +10,8 @@ namespace CashManager.Model
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool isCosts { get; set; }
         public int? Price { get; set; }
+       
     }
 }
